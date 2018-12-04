5f631c3ffbb28e017575e83e560ce33f9cfc6bdd
barbaramironova <barbara04mir@gmail.com>
2018-12-05 00:49:55 +0300

    Last version of Main window widget

---
+++ MainWindowTest.py
@@ -0,0 +1,19 @@
+from Ui_MainWindow import Ui_MainWindow
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QLabel
+
+class MyWidget(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.surprise = QLabel(self)
+        self.surprise.move(300, 300)
+        self.plusBatton.clicked.connect(self.run)
+
+    def run(self):
+        self.surprise.setText('Surprise')
+
+app = QApplication(sys.argv)
+ex = MyWidget()
+ex.show()
+sys.exit(app.exec_())
