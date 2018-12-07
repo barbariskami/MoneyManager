94e5752fe268c2e80f45b2e389c8fbb71e80fd80
barbaramironova <barbara04mir@gmail.com>
2018-12-08 00:43:15 +0300

    .

---
+++ OpenFile.py
@@ -0,0 +1,15 @@
+import os, json
+
+
+def open_file(name):
+    if not os.path.exists(name):
+        data = {'Sum': 0, 'Operations': [], 'Payments': []}
+    else:
+        f = open(name, encoding='utf8').read()
+        data = json.loads(f)
+        print(data)
+    return data
+
+
+print(open_file('hello.txt'))
+
