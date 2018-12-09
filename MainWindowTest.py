79237e54a5bbea7a9037ae570fdd3a08e655f3af
barbaramironova <barbara04mir@gmail.com>
2018-12-09 17:37:05 +0300

    Окончена работа с функцией добавления платежа

--- MainWindowTest.py
+++ MainWindowTest.py
@@ -2,13 +2,19 @@ from MainWindowNew import Ui_MainWindow
 from AddOperationWidget import Operation_Add_Widget
 from OpenFile import open_file
 import sys
+import datetime
 from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QLabel, QInputDialog
+from PyQt5.QtCore import QDate
 
 
 class Adding_Widget(Operation_Add_Widget, QWidget):
     def __init__(self):
         super().__init__()
 
+        now = str(datetime.datetime.now()).split()[0].split('-')
+        today_date = QDate()
+        today_date.setDate(int(now[0]), int(now[1]), int(now[2]))
+        #self.dateEdit.setDate(today_date)
 
 
 class MyWidget(QMainWindow, Ui_MainWindow):
@@ -20,6 +26,8 @@ class MyWidget(QMainWindow, Ui_MainWindow):
 
         self.FileName, okButtonPressed = QInputDialog.getText(self, 'Open file', 'Укажите путь к файлу')
         if okButtonPressed:
+            self.data = open_file(self.FileName)
+        else:
             self.data = open_file(self.FileName)
 
         self.SumLabel.setText(str(self.data['Sum']))
@@ -34,16 +42,24 @@ class MyWidget(QMainWindow, Ui_MainWindow):
         self.Window.SaveButton.clicked.connect(self.operation_saving)
 
     def operation_saving(self):
-        money = self.Window.SumEdit.text()
+        money = int(self.Window.SumEdit.text())
         operation = '-' if self.Window.MinusButton.isChecked() else '+'
-        date = self.Window.dateEdit.date()
+        date = self.Window.dateEdit.date().getDate()
         name = self.Window.NameEdit.text()
         comment = self.Window.CommentEdit.text()
         categories = [i.strip().lower() for i in self.Window.CategoriesEdit.text().split(',')]
         self.data['Operations'].append(
             {'money': money, 'operation': operation, 'date': date, 'name': name, 'comment': comment,
              'categories': categories})
+        sum = self.data['Sum']
+        if operation == '-':
+            sum -= money
+        else:
+            sum += money
+        self.data['Sum'] = sum
+        self.SumLabel.setText(str(self.data['Sum']))
         print(self.data)
+        self.Window.close()
 
 
 app = QApplication(sys.argv)
